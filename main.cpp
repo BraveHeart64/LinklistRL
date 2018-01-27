4363d7fd0b7f351f2e9cac37e18e86b6a236d8f2
Ryan <civmaster64@gmail.com>
2018-01-27 12:11:04 -0600

    Add files via upload
    Took a while but I prevailed. I was able to recreate most of this code in 30 minutes because of practice. however, I did struggle with the deletion of the node because I forgot to reset the temp to the next pointer. I used a pointer to pointer reference to finished off head Node  Originally, I put the node in a class but decided that it was better to just use a structer because Classes  automatically private in C++ while structures are public.

---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+
+
+struct Node {
+	Node* next;
+	int value;
+};
+
+
+class linklist {
+private:
+	Node * head, *tail;
+	Node** ref_to_head;
+
+
+public:
+	void add(int val);
+	void print();
+	void del();
+
+	linklist();
+	~linklist();
+};
+
+
+linklist::linklist() {
+	head = 0;
+	tail = 0;
+
+}
+
+linklist::~linklist() {
+	del();
+}
+
+void linklist::add(int val){
+	Node* temp = new Node();
+	temp->value = val;
+
+	if (head == 0) {
+		head = temp;
+		tail = temp;
+	}
+	else {
+		tail->next = temp;
+		tail = tail->next;
+	}
+
+
+
+}
+
+void linklist::del() {
+	Node* temp = new Node();
+	Node* next;
+	ref_to_head = &head;
+	
+	while (temp != 0) {
+		next = temp->next;
+		delete temp;
+		temp = next;
+	}
+
+	*ref_to_head = 0;
+
+
+
+
+}
+
+
+
+void linklist::print() {
+	Node* temp = new Node();
+	temp = head;
+	while (temp != 0) {
+		cout << temp->value<<endl;
+		temp = temp->next;
+	}
+
+}
+
+
+int main(int argc, char* argv[]) {
+	linklist la;
+	la.add(10);
+	la.add(20);
+	la.add(30);
+	la.add(40);
+	la.add(50);
+	la.print();
+	cin.get();
+
+	return 0;
+}
